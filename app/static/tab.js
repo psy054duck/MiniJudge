11bf87018a27f7b3f2a06333c874450023a44d3a
psy054duck <wangchlin0@163.com>
2018-04-06 00:42:34 +0800

    fix bug

--- app/static/tab.js
+++ app/static/tab.js
@@ -1,12 +1,14 @@
-code = document.getElementById('code_area');
+code = document.getElementsByClassName('code_area');
 
-code.addEventListener('keydown', function(e) {
-    if(e.keyCode===9){
-        var position=this.selectionStart+4;//此处我用了两个空格表示缩进，其实无所谓几个，只要和下面保持一致就好了。
-        this.value=this.value.substr(0,this.selectionStart)+'    '+this.value.substr(this.selectionStart);
-        this.selectionStart=position;
-        this.selectionEnd=position;
-        this.focus();
-        e.preventDefault();
-      }
-}, false);
+for (var i = 0; i < code.length; i++) {
+    code[i].addEventListener('keydown', function(e) {
+        if(e.keyCode===9){
+            var position=this.selectionStart+4;//此处我用了两个空格表示缩进，其实无所谓几个，只要和下面保持一致就好了。
+            this.value=this.value.substr(0,this.selectionStart)+'    '+this.value.substr(this.selectionStart);
+            this.selectionStart=position;
+            this.selectionEnd=position;
+            this.focus();
+            e.preventDefault();
+          }
+    }, false);
+}
